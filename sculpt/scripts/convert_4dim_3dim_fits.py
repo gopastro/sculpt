e83f1a0875e7513c92a46cd6fa5e58bda955a2ce
Gopal Narayanan <gopal@astro.umass.edu>
2015-08-13 10:35:38 -0400

    Add gildas converter for Supercam data

---
+++ sculpt/scripts/convert_4dim_3dim_fits.py
@@ -0,0 +1,30 @@
+"""
+Convert 4 dimensional FITS cube files 
+produced by GILDAS to 3 dimensional
+FITS cubes
+"""
+from astropy.io import fits as pyfits
+from sculpt.idealpy.radio import transpose_cube
+
+def convert_gildas_and_transpose(filename,
+                                 output_filename=None):
+    """
+    Takes a 4d GILDAS file and converts it first 
+    into a 3d xyv format cube, and then transposes
+    to 3d vxy format and saves output
+    """
+    if output_filename is None:
+        output_filename = filename.lower()
+    hdulist = pyfits.open(filename)
+    hdu = hdulist[0]
+    header = hdu.header
+    data = hdu.data
+    data.shape = data.shape[1:]
+    header['NAXIS'] = 3
+    for attr in ('NAXIS4', 'CTYPE4', 'CRVAL4',
+                 'CRPIX4', 'CROTA4', 'CDELT4'):
+        del header[attr]
+    hduout = transpose_cube(hdu, origin='xyv')
+    hdulist = pyfits.HDUList([hduout])
+    print "Writing converted FITS file to %s" % output_filename
+    hdulist.writeto(output_filename)
