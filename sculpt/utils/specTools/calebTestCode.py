922de8c93c6f6444d007f284904008a3d584e68c
Caleb Wheeler <chw3k5@gmail.com>
2015-08-14 13:29:05 -0700

    Caleb and Teddy's merged spectral analysis tools. These operate on spectra only and do not yet include HDU data.

--- sculpt/utils/specTools/calebTestCode.py
+++ sculpt/utils/specTools/calebTestCode.py
@@ -8,13 +8,7 @@ from astropy.modeling import models, fitting
 def getSpecDataCube(filename,):
     if getpass.getuser() == 'chw3k5':
         dirname = '/Users/chw3k5/Documents/Grad_School/supercamG30data/'
-
-    elif getpass.getuser() == 'theodore':
-	dirname = '/home/theodore/astroenv/supercamdata/supercamG30data/'
-    else:
-        dirname = ''
-    filename = dirname+filename
-
+        filename = dirname+filename
     HDU = pyfits.open(filename)
     fitsData = HDU[0]
     specDataCube = fitsData.data[0,:,:,:] # The 0 is the polarization axis
@@ -51,69 +45,149 @@ def findHighIntensitySpec(specDataCube, plotData=False):
         plt.show()
     return (val_galLat,mean_galLon), (val_galLat,mean_galLon)
 
-def test_for_multiple_peaks(specdatacube, pos_x, pos_y, noise_threshhold, rmsnoise = None, test=False):
-    # this function initially draws a single gaussian to fit the peak
-    # if the residuals are then anti-symmetric about this new peak
-    # we then re-fit the original spectra with two gaussians
 
-    #probably only works with spectra with a single major peak
-    if test:
-        x = numpy.arange(850)
-        peak1 = 6.5 * numpy.exp(-numpy.power(x - 284, 2.) / (2 * numpy.power(10, 2.)))
-        peak2 = 10.0 * numpy.exp(-numpy.power(x - 270, 2.) / (2 * numpy.power(4, 2.)))
-        spectra = peak1 + peak2 + numpy.random.normal(0.0, size = len(x)) * 1.2
-    else:
-        spectra = specdatacube[:,pos_x,pos_y]
-        x = numpy.arange(len(spectra))
+def testSpecPlotting(testSpec,x, gg_fit, gg_init, showPlot=True):
+    plt.plot(x, testSpec, 'ko')
+    plt.plot(x, gg_fit(x), 'r-', lw=4)
 
-    guess = numpy.where(spectra == spectra.max())[0]
-    std = 5.0  #just a guess for now
-    init_model = models.Gaussian1D(spectra.max(),guess,std)
-    fitter_init = fitting.SLSQPLSQFitter()
-    fit_init = fitter_init(init_model,x,spectra)
-    residuals_init = spectra - fit_init(x)
-    #plt.plot(x, spectra)
-    #plt.plot(x, fit_init(x))
-    #plt.plot(x, residuals_init)
-    if (rmsnoise == None):
-        rmsnoise = residuals_init.std()
-    before = numpy.mean(residuals_init[guess-6:guess])
-    b_sign = numpy.sign(before)
-    after = numpy.mean(residuals_init[guess:guess+6])
-    a_sign = numpy.sign(after)
-    print (rmsnoise,before, after)
-    if ((numpy.sign(before) != numpy.sign(after))):
-        if ((abs(before) >= rmsnoise) or (abs(after) >= rmsnoise)):
-            print "there might be two lines here"
-            #time to fit the residuals
-            if (b_sign == 1):
-                resid_model = models.Gaussian1D(spectra.max() / 10.0, guess - 3, std) -  models.Gaussian1D(spectra.max() / 10.0, guess + 3, std)
-            else:
-                resid_model = models.Gaussian1D(spectra.max() / 10.0, guess + 3, std) -  models.Gaussian1D(spectra.max() / 10.0, guess - 3, std)
-            fitter_resid = fitting.SLSQPLSQFitter()
-            fit_resid = fitter_resid(resid_model, x, residuals_init)
-            #now that we've fit the residuals, we'll use this as a seed for the final 2-line fit
-            final_gauss1 = models.Gaussian1D(spectra.max() / 2.0, fit_resid[0].mean[0][0], fit_resid[0].stddev[0])
-            final_gauss2 = models.Gaussian1D(spectra.max() / 2.0, fit_resid[1].mean[0][0], fit_resid[1].stddev[0])
-            final_model = final_gauss1 + final_gauss2
-            fitter_final = fitting.SLSQPLSQFitter()
-            fit_final = fitter_final(final_model, x, spectra)
-            plt.figure(1)
-            plt.subplot(211)
-            plt.plot(x, spectra, label = "Spectra")
-            plt.plot(x, fit_init(x), label = "1 Line Fit")
-            plt.legend()
-            plt.subplot(212)
-            plt.plot(x, spectra, label = "Spectra")
-            plt.plot(x, fit_final(x), label = "2 Line Fit")
-            plt.plot(x, fit_final[0](x), label = "Gaussian 1")
-            plt.plot(x, fit_final[1](x), label = "Gaussian 2")
+    for initmodelIndex in range(len(gg_init.parameters)/3):
+        baseIndex=initmodelIndex*3
+        model_ginit = models.Gaussian1D(gg_init.parameters[baseIndex],
+                                        gg_init.parameters[baseIndex+1],
+                                        gg_init.parameters[baseIndex+2])
+        plt.plot(model_ginit(x),'b-', lw=1)
+
+    # for submodel in gg_fit._submodels:
+    #     plt.plot(submodel(x),'b-', lw=1)
+    for modelIndex in range(len(gg_fit._parameters)/3):
+        baseIndex=modelIndex*3
+        model_g = models.Gaussian1D(gg_fit._parameters[baseIndex],
+                                    gg_fit._parameters[baseIndex+1],
+                                    gg_fit._parameters[baseIndex+2])
+        plt.plot(model_g(x),'g-', lw=1)
+    if showPlot:plt.show()
+    return
+
+def getSpecLocalMaxima(inputSpec, doConv=False,sigma=5.0,convRadius=10,doPlot=False,minSignifigance=0.5):
+    """
+    This function is used to find the local maxima of a spectrum
+    :param inputSpec: should just be a 1D array of regularly spaced spectral data
+    :param doConv: changes the behavior of the function to include a convolution with a gaussian kernel
+    :type doConv: bool
+    :param sigma: in units of the regular spacing of the function parameter inputSpec and is the sigma used to specify
+    the sigma of the gaussian convolution kernel to be applied to the data in the array inputSpec
+    :type sigma: float
+    :param convRadius: in units of the regular spacing of the function parameter inputSpec, this is the radius of the
+    gaussian convolution kernel to be applied to the data in the array inputSpec. The actual size of the kernel is
+    2*convRadius+1
+    :type convRadius: int
+    :param minSignifigance: in units of the regular spacing of the function parameter inputSpec, this is the minimum
+    number of standard deviations that that a maxima can be from zero and still be returned by this function
+    :return final_maximaList: this is a list of tuples the is the form (index_of_maxima,value_of_maxima)
+    """
+    if doConv:
+        convRadius=abs(convRadius)
+        convX = range(-1*convRadius,convRadius+1)
+        lostConvLen = convRadius
+        g = models.Gaussian1D(1, 0, sigma)
+        convSpec = numpy.convolve(inputSpec,g(convX),'valid')
+        convSpec*=numpy.mean(inputSpec)/numpy.mean(convSpec)
+        testSpec=convSpec
     else:
-        plt.plot(x, spectra, label = "Spectra")
-        plt.plot(x, fit_init(x), label = "1 Line Fit")
-    plt.legend()
+        testSpec=inputSpec
+        lostConvLen=0
+
+    maximaTruthArray = numpy.r_[True, testSpec[1:] > testSpec[:-1]] & numpy.r_[testSpec[:-1] > testSpec[1:], True]
+    maximaList=[]
+    for maximaIndex in range(len(maximaTruthArray)):
+        isMaxima = maximaTruthArray[maximaIndex]
+        if isMaxima:
+            x_val = maximaIndex+lostConvLen
+            test_val = testSpec[maximaIndex]
+            maximaList.append((x_val,test_val))
+    maximaList.sort(key=lambda tup: tup[1])
+    maximaList.reverse()
+
+    if doConv:
+        residual = inputSpec[convRadius:-convRadius]-convSpec
+    else:
+        residual = inputSpec
+    residual_Error = numpy.std(residual)
+    lowerCutoff = residual_Error*minSignifigance
+    final_maximaList=[]
+    for maxima in maximaList:
+        (x_val,test_val) = maxima
+        if lowerCutoff <= test_val:
+            final_maximaList.append(maxima)
+
+    if doPlot:
+        x_lenInputSpec = len(inputSpec)
+        x_inputSpec = range(x_lenInputSpec)
+        plt.plot(x_inputSpec, inputSpec, 'k-')
+        if doConv:
+            x_conv = range(lostConvLen,len(convSpec)+lostConvLen)
+            plt.plot(x_conv, convSpec, 'g-',linewidth=3)
+            x_res=x_conv
+        else:
+            x_res=x_inputSpec
+        x_resLen = len(x_res)
+        plt.plot(x_res, residual, color='darkorchid')
+        plt.plot(x_res, residual_Error*numpy.ones(x_resLen), color = 'dodgerblue')
+        plt.plot(x_res, lowerCutoff*numpy.ones(x_resLen), color = 'orchid')
+        for (x_val, test_val) in maximaList:plt.plot(x_val,test_val,'ro')
+        for (x_val, test_val) in final_maximaList:plt.plot(x_val,test_val,'go')
+        plt.show()
+    return final_maximaList
+
+
 if __name__ == "__main__":
+    # Getting data here and selecting a test spectrum
     filename = 'G30_Map_a.fits'
-    specdatacube, HDU = getSpecDataCube(filename=filename)
-    test_for_multiple_peaks(specdatacube, 370, 122, 3.0)
-    plt.show()
+    specDataCube, HDU = getSpecDataCube(filename=filename)
+    testSpec = specDataCube[:,342,122]
+
+    fractionalRimprovment = 0.005
+    showMaximaPlot = False
+    maximaConvSigma = 5.
+    maximaConvRadius = 20
+
+    showFitterPlots = True
+    # initialize the model
+    maximaList = getSpecLocalMaxima(testSpec,doConv=True,sigma=maximaConvSigma,convRadius=maximaConvRadius,doPlot=showMaximaPlot)
+    len_spec = len(testSpec)
+    x = range(len_spec)
+    if maximaList == []:
+        print 'No local maxima were found'
+        x_mean = float(raw_input('Enter the channel number in range:('+str(min(x))+','+str(max(x))+')'))
+        x_apml = float(raw_input('Enter the amplitude in range:('+str(min(testSpec))+','+str(max(testSpec))+')'))
+    else:
+        (x_mean,x_apml) = maximaList[0]
+    g_first = models.Gaussian1D(x_apml, x_mean, 5)
+    fitter = fitting.SLSQPLSQFitter()
+    g_firstFit = fitter(g_first,x,testSpec)
+    fit_info = fitter.fit_info
+    R_val = fit_info['final_func_val']
+    first_Rvalue = R_val
+    R_val_current = R_val
+    if showFitterPlots: testSpecPlotting(testSpec,x,g_firstFit,g_first, showPlot=True)
+    modelParamsList = [(g_firstFit.parameters[0],g_firstFit.parameters[1],g_firstFit.parameters[2])]
+    current_models = models.Gaussian1D(modelParamsList[0])
+
+    # loop through and see if it is worth trying to fit to the other local maxima
+    gg_init=models.Gaussian1D(modelParamsList[0])
+    len_maximaList = len(maximaList)
+    if 1 < len_maximaList:
+        for loopIndex in range(1,len_maximaList):
+            R_last = R_val_current
+            (x_mean,x_apml) = maximaList[loopIndex]
+            gg_init=current_models+models.Gaussian1D(x_apml, x_mean, 5)
+            gg_Fit = fitter(gg_init,x,testSpec)
+            fit_info = fitter.fit_info
+            R_val_current = fit_info['final_func_val']
+            if showFitterPlots: testSpecPlotting(testSpec,x,gg_Fit,gg_init, showPlot=True)
+            if R_val_current <= R_last*(1.-fractionalRimprovment):
+                modelParamsList.append((gg_Fit.parameters[0],gg_Fit.parameters[1],gg_Fit.parameters[2]))
+                current_models += models.Gaussian1D(modelParamsList[loopIndex])
+            else:
+                break
+
