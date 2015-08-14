e020ab297bbe2aba624310a916e11a83a68c9b10
Gopal Narayanan <gopal@astro.umass.edu>
2015-08-14 10:25:18 -0400

    Bug fix for name of spectrum

--- sculpt/plots/sculpture.py
+++ sculpt/plots/sculpture.py
@@ -229,7 +229,7 @@ class Sculpture(SculptureFITSFigure):
                 title = 'HDU 0'
             else:
                 title = self.extra_hdu_titles[i]
-            specax.plot(vel, co12, linestyle='steps-middle', label=title)
+            specax.plot(vel, spec, linestyle='steps-middle', label=title)
         specax.set_title('Spectrum at %.1f, %.1f' % (xdata, ydata))
         specax.legend(loc='best')
         self.specfigure.canvas.draw()    
